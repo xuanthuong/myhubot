862d3c13c97f1389e6cc7c0caead070972b4dddd
ThuongTran <xuanthuongtran@gmail.com>
2017-09-25 14:16:56 +0700

    add game bot

---
+++ scripts/gamebot.js
@@ -0,0 +1,80 @@
+const request = require('request')
+
+// Notice Image to RocketChat
+let image = {
+  "well_done": "https://thumbs.dreamstime.com/b/well-done-23443783.jpg",
+  "supper": "https://cdn.schoolstickers.com/products/en/819/128740-05.png",
+  "great_job": "https://sites.create-cdn.net/siteimages/37/9/9/379961/12/9/0/12909934/450x430.jpg?1469478215",
+  "great_effort": "https://www.thenaughtyseat.co.uk/ekmps/shops/rescapeltd/images/great-effort-well-done-stickers-2155-p.png",
+  "fighting": "http://bec.edu.vn/rezise/resize?src=http://bec.edu.vn/asset/upload/bai_hoc/han_quoc/150416_success_kid_10.jpg&w=760&h=400",
+  "excellent": "https://cdn.schoolstickers.com/products/en/819/128740-08.png",
+  "do_your_best": "http://www.swiss-miss.com/wp-content/uploads/2012/09/Screen-Shot-2012-09-10-at-10.28.44-PM-480x191.png",
+  "co_len_tieng_han": "http://trungtamtienghan.edu.vn/uploads/news/2017_02/co-len-tieng-han-phien-am.jpg",
+  "work_hard_play_hard": "https://cdn-media-1.lifehack.org/wp-content/files/2017/04/18232920/work-hard-play-hard.001-370x208@2x.jpeg",
+  "you_can_do_it": "http://khmer-online.com/wp-content/uploads/2016/09/you-can-do-it.png"
+}
+
+let api_url = "http://dounets.com:5002/rocketchat/notify-game-results"
+
+module.exports = function(robot) {
+  robot.hear(/notice result/i, function(res) {
+
+    var keys = Object.keys(image)
+
+    let image_url = image[keys[ keys.length * Math.random() << 0]]
+    
+    notice = {
+        "roomId": "thuongtran",
+        "text": "Today game result",
+        "alias": "Boss",
+        "emoji": ":smirk:",
+        "avatar": "https://i.pinimg.com/originals/d7/b1/8c/d7b18cfeb93cac2ea7f75f8951f240f1.gif",
+        "attachments": [{
+            "color": "#ff0000",
+            "author_icon": "https://i.pinimg.com/originals/d7/b1/8c/d7b18cfeb93cac2ea7f75f8951f240f1.gif",
+            "author_name": "",
+            "author_link": "",
+            "ts": "",
+            "title": "Game Result Notification",
+            "title_link": "https://dounets.com:5003",
+            "title_link_download": "",
+            "text": "",
+            "thumb_url": "",
+            "message_link": "https://google.com",
+            "collapsed": false,
+            "image_url": image_url,
+            "audio_url": "",
+            "video_url": "",
+            "fields": [{
+                "short": true,
+                "title": "Hole result",
+                "value": "[Link](http://dounets.com:5003/golfgame-api/notice-hole-result) go to hole results."
+            },{
+                "short": true,
+                "title": "Game result",
+                "value": "[Link](http://dounets.com:5003/golfgame-api/notice-game-result) go to game results."
+            }]
+        }]
+    }
+
+    request.post({
+      url: api_url,
+      json: notice
+      }, 
+      function(error, response, body){
+        console.log(body)
+      })
+    
+    // notice = JSON.stringify(notice)
+
+    // return robot.http(api_url).header('Content-Type', 'application/json').post(JSON.stringify(notice))(
+    //   function(err, _res, body) {
+    //     if (err) {
+    //       res.send("Could not submit leave request, please try again!");
+    //       return
+    //     }
+    //     // return res.send(msg)
+    //     return "ok"
+    // })
+  })
+}
--- scripts/hellobot.js
+++ scripts/hellobot.js
@@ -16,18 +16,6 @@ var options = {
     args: ['hello']
 };
 
-// function askBot(question){
-//   options.args = question
-//   ans = "I dont understand, please ask again"
-//   PythonShell.run(brobotScript, options, function (err, results) {
-//     if (err) throw err;
-//     // results is an array consisting of messages collected during execution
-//     console.log('results: %j', results);
-//     ans = results;
-//   }); 
-//   return ans;
-// }
-
 const askBot = (question) => {
   return new Promise((resolve, reject) => {
     options.args = question
@@ -41,26 +29,22 @@ const askBot = (question) => {
 }
 
 module.exports = function(robot) {
-  robot.hear(/Which chatting platform should we use/i, function(res) {
-    return res.send("@" + res.envelope.user.name + ", I suggest to use RocketChat, Hubot will be very helpful to your members");
-  });
   robot.hear(/(Hello\s*$)|(Hi\s*$)/i, function(res) {
     msg = "Hi @" + res.envelope.user.name + ", how I help you?"
-    // say.speak(msg)
-    // return res.send(msg);
+    return res.send(msg);
     // room = res.envelope.user.name
     // console.log(`first room: ${room}`)
     // robot.adapter.sendDirect( 'thuongtran' , "thuongtran, Hello how are you?")
     // room = robot.adapter.client.rtm.dataStore.getDMByName(res.envelope.envelope.name)
     // robot.messageRoom(room.id, "Hello, this is a private message!")
     // console.log(`room: ${room}`)
-    askBot("I am engineering")
-      .then((msg) => {
-        // console.log('msg: %j', msg);
-        res.send(msg.join(' '))
-      })
-      .catch((error) => {
-        throw error
-      }) 
-  });
-};
+    // askBot("I am engineering")
+    //   .then((msg) => {
+    //     // console.log('msg: %j', msg);
+    //     res.send(msg.join(' '))
+    //   })
+    //   .catch((error) => {
+    //     throw error
+    //   })
+  })
+}
